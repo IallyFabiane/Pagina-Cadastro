8e1d0d835d0c3f1a931ed9b10644a6fe02b5997f
Ially Fabiane <iallyfabiane@gmail.com>
2022-03-09 21:33:57 -0300

    adicionado repositorio

---
+++ main.js
@@ -0,0 +1,40 @@
+let listaDeInputs = document.querySelectorAll('.resultado');
+for (let indice = 0; indice < listaDeInputs.length; indice++) {
+   let input = listaDeInputs[indice];
+   let valor = input.value;
+   input.onkeydown = valor; 
+   console.log(valor);
+}
+
+let senhaDeAcesso = 4568659;
+let inputSenha = document.getElementById('senha');
+let senhaComputada;
+let acessoNegado = false;
+
+function acesso (senha) {
+   senhaComputada = inputSenha.value;
+      if (senhaComputada == senhaDeAcesso) {
+         return alert('Acesso permitido.');
+      } else {
+         return prompt ('Acesso negado. Tente novamente aqui: ');
+      } 
+} 
+
+function tentativasDeAcesso () {
+   let tentativas = 0;
+   while (tentativas < 3) {
+      if (senhaDeAcesso != senhaComputada) {
+         tentativas++;
+         acesso(senhaComputada);
+      } else { 
+        break;
+      } 
+   } 
+}
+
+let inputSubmit = document.querySelector('#envio');
+inputSubmit.onclick = tentativasDeAcesso;
+
+
+
+   
